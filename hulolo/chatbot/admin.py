4452cb73f01238388ce4081d09ed072806099d6e
MDChain <a26831302@gmail.com>
2026-06-22 13:14:43 +0800

    fix: upload the example files and code

--- hulolo/chatbot/admin.py
+++ hulolo/chatbot/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 from .models import Course
 from import_export.admin import ImportExportModelAdmin
 
+# 程式碼 7-25
+from .models import CourseAlias
+
+# 程式碼 6-7
 @admin.register(Course)
 class CourseAdmin(ImportExportModelAdmin):
     list_display = ('teacher_name','course_name')
+
+# 程式碼 7-25
+@admin.register(CourseAlias)
+class CourseAliasAdmin(ImportExportModelAdmin):
+    list_display = ('course_name', 'alias')
